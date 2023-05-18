df7914e0bed934f037c28578c667a2f554430c3b
aleksanderpanasiuk <olekp249@gmail.com>
2023-05-18 23:22:44 +0200

    parameters are now saved to file every 100 generations

--- main.cpp
+++ main.cpp
@@ -11,32 +11,32 @@
 int main()
 {
     int creatures_to_kill = 100;
-    int number_of_generations = 10;
+    int number_of_generations = 1000;
     Points points(10);
     points.randomize_points();
     points.read_points();
 
     Parameters parameters;
     parameters.fill_random();
-    parameters.write_to_file();
-    parameters.read_from_file();
 
     std::cout << points;
 
-    for (int i = 0; i < number_of_generations; i++)
+    for (int i = 0; i <= number_of_generations; i++)
     {
-        // std::cout << i << " gen: \n";
-        // parameters.print_top_parameters(3, true);
-
         parameters.kill_bottom(creatures_to_kill);
-        parameters.pairing(creatures_to_kill);
         parameters.calculate_fitness(points.get_number_of_points(), points.points);
         parameters.fitness_sort();
+        if (i%100 == 0)
+        {
+            std::string file_path = "resources/generations/parameters_" +
+                std::to_string(i) + ".csv";
+            parameters.write_to_file(file_path);
+        }
     }
 
     std::cout << number_of_generations << " gen: \n";
     parameters.print_top_parameters(3, true);
-    parameters.write_to_file();
+    parameters.write_to_file("resources/parameters.csv");
 
     return 0;
 }
