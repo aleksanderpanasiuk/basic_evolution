2b6b2dcdb6150dceeae8fe83b23cdd1450ea87f9
aleksanderpanasiuk <olekp249@gmail.com>
2023-05-18 21:00:19 +0200

    Added points randomization

--- main.cpp
+++ main.cpp
@@ -11,14 +11,17 @@
 int main()
 {
     int creatures_to_kill = 100;
-    int number_of_generations = 1000;
+    int number_of_generations = 10;
     Points points(10);
+    points.randomize_points();
     points.read_points();
 
     Parameters parameters;
     parameters.fill_random();
     parameters.write_to_file();
     parameters.read_from_file();
+
+    std::cout << points;
 
     for (int i = 0; i < number_of_generations; i++)
     {
@@ -33,6 +36,7 @@ int main()
 
     std::cout << number_of_generations << " gen: \n";
     parameters.print_top_parameters(3, true);
+    parameters.write_to_file();
 
     return 0;
 }
