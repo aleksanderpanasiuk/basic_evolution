0330cb0d695d8ed353a395064fb676b506823e27
aleksanderpanasiuk <olekp249@gmail.com>
2023-05-19 00:27:34 +0200

    Changed the way graphs are displayed

--- display.py
+++ display.py
@@ -3,16 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def show(number_of_generations):
+figure, axis = plt.subplots(2, 2)
+
+
+def position(n):
+    if n == 0:
+        return 0, 0
+    if n == 1:
+        return 0, 1
+    if n == 2:
+        return 1, 0
+    if n == 3:
+        return 1, 1
+
+
+def show(n, number_of_generations):
+    pos_x, pos_y = position(n-2)
+
     points = pd.read_csv("resources/points.csv", header=0)
-    points.plot.scatter(x=0, y=1)
+    points_x = list(points["x"])
+    points_y = list(points["y"])
+    axis[pos_x, pos_y].plot(points_x, points_y, 'o')
+    legend = ["Points"]
 
     parameters = pd.read_csv(
         f"resources/generations/parameters_{number_of_generations}.csv",
         header=0)
 
     x = np.linspace(0, 10, 100)
-    legend = ["Points"]
 
     for i in range(3):
         par = list(parameters.loc[i])
@@ -21,15 +39,15 @@ def show(number_of_generations):
             par[3] * np.sin((par[4]*x + par[5])*np.pi/180) + \
             par[6] * np.sin((par[7]*x + par[8])*np.pi/180) + par[9]
 
-        plt.title(f"{number_of_generations}")
-        plt.plot(x, y)
+        axis[pos_x, pos_y].plot(x, y)
         legend.append(f"Creature nr {str(i)}")
 
-    plt.legend(legend)
+    axis[pos_x, pos_y].set_title(f"Generation {number_of_generations}")
+    axis[pos_x, pos_y].legend(legend)
 
 
 if __name__ == "__main__":
-    show(20)
-    show(60)
-    show(100)
+    for i in range(2, 6):
+        show(i, i*20)
+
     plt.show()
